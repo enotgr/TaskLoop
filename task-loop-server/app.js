aa264ddb2c669fcf592046c108ab76876c5d0cfc
Mikhail Krotov <genry.foer@gmail.com>
2020-12-05 20:12:48 +0300

    add mongo db connect

--- task-loop-server/app.js
+++ task-loop-server/app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+// create config with your MONGO_URI for working with your mongoDB
+const appConfig = require('./app.config');
 
 const authRoutes = require('./routes/auth');
 
 const app = express();
+
+mongoose.connect(appConfig.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected.');
+  })
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
